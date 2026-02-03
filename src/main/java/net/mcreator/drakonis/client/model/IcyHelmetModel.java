6c87d98b897c52a00e4222c90b4ffc01e14cb07e
unknown <{texnobola2@gmail.com}>
2026-02-03 17:04:30 +0500

    floating helmet error fixed

--- src/main/java/net/mcreator/drakonis/client/model/IcyHelmetModel.java
+++ src/main/java/net/mcreator/drakonis/client/model/IcyHelmetModel.java
@@ -31,50 +31,50 @@ public class IcyHelmetModel<T extends Entity> extends EntityModel<T> {
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
 		PartDefinition helmet = partdefinition.addOrReplaceChild("helmet", CubeListBuilder.create()
-				.texOffs(0, 0).addBox(-4.5F, -36.0F, -4.5F, 9.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(0, 0).addBox(-4.0F, -38.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
-				.texOffs(0, 0).addBox(-5.0F, -33.0F, -5.0F, 1.0F, 3.0F, 6.0F, new CubeDeformation(0.0F))
-				.texOffs(0, 0).addBox(4.0F, -33.0F, -5.0F, 1.0F, 3.0F, 6.0F, new CubeDeformation(0.0F))
-				.texOffs(0, 0).addBox(-2.0F, -42.0F, -4.0F, 4.0F, 4.0F, 5.0F, new CubeDeformation(0.0F))
-				.texOffs(0, 0).addBox(-1.0F, -45.0F, -3.0F, 2.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
-				.texOffs(0, 0).addBox(-3.0F, -33.0F, -4.2F, 2.0F, 1.0F, 0.3F, new CubeDeformation(0.0F))
-				.texOffs(0, 0).addBox(1.0F, -33.0F, -4.2F, 2.0F, 1.0F, 0.3F, new CubeDeformation(0.0F)),
-				PartPose.offset(0.0F, 50.0F, 0.0F));
+				.texOffs(0, 0).addBox(-4.5F, 14.0F, -4.5F, 9.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 0).addBox(-4.0F, 12.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 0).addBox(-5.0F, 17.0F, -5.0F, 1.0F, 3.0F, 6.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 0).addBox(4.0F, 17.0F, -5.0F, 1.0F, 3.0F, 6.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 0).addBox(-2.0F, 8.0F, -4.0F, 4.0F, 4.0F, 5.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 0).addBox(-1.0F, 5.0F, -3.0F, 2.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 0).addBox(-3.0F, 17.0F, -4.2F, 2.0F, 1.0F, 0.3F, new CubeDeformation(0.0F))
+				.texOffs(0, 0).addBox(1.0F, 17.0F, -4.2F, 2.0F, 1.0F, 0.3F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 0.0F, 0.0F));
 
 		PartDefinition Back_Plate_Upper_r1 = helmet.addOrReplaceChild("Back_Plate_Upper_r1",
 				CubeListBuilder.create()
 						.texOffs(0, 0).addBox(-3.0F, -3.0F, -0.5F, 6.0F, 5.0F, 1.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(0.0F, -36.0F, 4.0F, 0.3927F, 0.0F, 0.0F));
+				PartPose.offsetAndRotation(0.0F, 14.0F, 4.0F, 0.3927F, 0.0F, 0.0F));
 
 		PartDefinition Back_Plate_Lower_r1 = helmet.addOrReplaceChild("Back_Plate_Lower_r1",
 				CubeListBuilder.create()
 						.texOffs(0, 0).addBox(-4.0F, -4.0F, 0.0F, 8.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(0.0F, -30.0F, 4.0F, -0.3927F, 0.0F, 0.0F));
+				PartPose.offsetAndRotation(0.0F, 20.0F, 4.0F, -0.3927F, 0.0F, 0.0F));
 
 		PartDefinition Horn_Tip_Right_r1 = helmet.addOrReplaceChild("Horn_Tip_Right_r1",
 				CubeListBuilder.create()
 						.texOffs(0, 0).addBox(-0.5F, -4.0F, -1.0F, 3.0F, 5.0F, 2.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(7.0F, -39.0F, -0.5F, 0.0F, 0.0F, -0.7854F));
+				PartPose.offsetAndRotation(7.0F, 11.0F, -0.5F, 0.0F, 0.0F, -0.7854F));
 
 		PartDefinition Horn_Base_Right_r1 = helmet.addOrReplaceChild("Horn_Base_Right_r1",
 				CubeListBuilder.create()
 						.texOffs(0, 0).addBox(0.0F, -4.0F, -1.0F, 3.0F, 4.0F, 3.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(4.0F, -35.0F, -1.0F, 0.0F, 0.0F, -0.3927F));
+				PartPose.offsetAndRotation(4.0F, 15.0F, -1.0F, 0.0F, 0.0F, -0.3927F));
 
 		PartDefinition Horn_Tip_Left_r1 = helmet.addOrReplaceChild("Horn_Tip_Left_r1",
 				CubeListBuilder.create()
 						.texOffs(0, 0).addBox(-2.5F, -4.0F, -1.0F, 3.0F, 5.0F, 2.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(-7.0F, -39.0F, -0.5F, 0.0F, 0.0F, 0.7854F));
+				PartPose.offsetAndRotation(-7.0F, 11.0F, -0.5F, 0.0F, 0.0F, 0.7854F));
 
 		PartDefinition Horn_Base_Left_r1 = helmet.addOrReplaceChild("Horn_Base_Left_r1",
 				CubeListBuilder.create()
 						.texOffs(0, 0).addBox(-3.0F, -4.0F, -1.0F, 3.0F, 4.0F, 3.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(-4.0F, -35.0F, -1.0F, 0.0F, 0.0F, 0.3927F));
+				PartPose.offsetAndRotation(-4.0F, 15.0F, -1.0F, 0.0F, 0.0F, 0.3927F));
 
 		PartDefinition Chin_Spike_r1 = helmet.addOrReplaceChild("Chin_Spike_r1",
 				CubeListBuilder.create()
 						.texOffs(0, 0).addBox(-2.0F, -2.0F, -1.0F, 4.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(0.0F, -30.0F, -5.0F, 0.3927F, 0.0F, 0.0F));
+				PartPose.offsetAndRotation(0.0F, 20.0F, -5.0F, 0.3927F, 0.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
